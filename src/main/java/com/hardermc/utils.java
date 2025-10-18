9f4454bf1b546ab23a1c472b9639c45940960aa3
Landon Redmond <landon.redmond0@gmail.com>
2025-10-17 22:07:28 -0500

    Add secondsToTicks utility method to convert seconds to ticks

--- src/main/java/com/hardermc/utils.java
+++ src/main/java/com/hardermc/utils.java
@@ -3,6 +3,12 @@ package com.hardermc;
 import org.bukkit.entity.Player;
 
 public class utils {
+    public static final int TICKS_PER_SECOND = 20;
+
+    public int secondsToTicks(int seconds) {
+        return seconds * TICKS_PER_SECOND;
+    }
+
     public int[] getRandomPositionAroundPlayer(Player player, int radius) {
         int x = player.getLocation().getBlockX() + (int) (Math.random() * radius) - radius / 2;
         int z = player.getLocation().getBlockZ() + (int) (Math.random() * radius) - radius / 2;

121bc4a4124ebfb1e3af7601370b618a1050440c
Landon Redmond <landon.redmond0@gmail.com>
2025-10-20 11:24:59 -0500

    Refactor MAX_ATTRIBUTE_VALUES to use Map.ofEntries for better organization and clarity

--- hardermc-plugin/src/main/java/com/hardermc/Utils.java
+++ hardermc-plugin/src/main/java/com/hardermc/Utils.java
@@ -1,15 +1,26 @@
 package com.hardermc;
 
+import java.util.List;
+import java.util.Map;
+
 import org.bukkit.Bukkit;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Mob;
 import org.bukkit.entity.Player;
 
 /** Utility class for common helper functions */
 public final class Utils {
     public static final int TICKS_PER_SECOND = 20;
-    public static final double MAX_ENTITY_HEALTH = 1024.0;
-    public static final double MAX_ENTITY_DAMAGE = 4086.0;
-    public static final double MAX_ENTITY_SPEED = 3.0;
+    public static final Map<Attribute, Double> MAX_ATTRIBUTE_VALUES = Map.ofEntries(
+            Map.entry(Attribute.MAX_HEALTH, 1024.0),
+            Map.entry(Attribute.ATTACK_DAMAGE, 2048.0),
+            Map.entry(Attribute.MOVEMENT_SPEED, 1024.0),
+            Map.entry(Attribute.ATTACK_SPEED, 1024.0),
+            Map.entry(Attribute.FOLLOW_RANGE, 2048.0),
+            Map.entry(Attribute.KNOCKBACK_RESISTANCE, 1.0),
+            Map.entry(Attribute.ARMOR, 30.0),
+            Map.entry(Attribute.ARMOR_TOUGHNESS, 20.0),
+            Map.entry(Attribute.LUCK, 1024.0));
 
     private Utils() {
     }
@@ -20,6 +31,10 @@ public final class Utils {
 
     public static <T> T randomEntryFromArray(T[] array) {
         return array[(int) (Math.random() * array.length)];
+    }
+
+    public static <T> T randomEntryFromList(List<T> list) {
+        return list.get((int) (Math.random() * list.size()));
     }
 
     public static int[] getRandomPositionAroundPlayer(Player player, int radius) {

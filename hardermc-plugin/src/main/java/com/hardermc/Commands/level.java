3802d9e2e8f9d261b0da1b4ed776213348024632
Landon Redmond <landon.redmond0@gmail.com>
2025-10-20 11:30:48 -0500

    Add level command to display current level and difficulty

---
+++ hardermc-plugin/src/main/java/com/hardermc/Commands/level.java
@@ -0,0 +1,26 @@
+package com.hardermc.Commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.hardermc.HarderMC;
+
+public class level implements CommandExecutor {
+    private final HarderMC plugin;
+
+    public level(HarderMC plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player))
+            return true;
+
+        sender.sendMessage(String.format("%d (%%d difficulty)", plugin.levelSystem.level,
+                (int) (plugin.levelSystem.levelMultiplier * 100)));
+        return true;
+    }
+}
